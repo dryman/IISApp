d4456501eb8e351f570f4fed99d869e802b00ae0
dryman <idryman@gmail.com>
2011-11-10 14:32:00 +0800

    DB::Result::Article use qw syntax instead of {key=>value}

--- lib/IISApp/DB/Result/Article.pm
+++ lib/IISApp/DB/Result/Article.pm
@@ -3,22 +3,12 @@ use base 'DBIx::Class::Core';
 
 __PACKAGE__->table('article');
 __PACKAGE__->add_columns(
-  id => { 
-    data_type => 'integer',
-    is_nullable => 0,
-    is_auto_increment => 1,
-  },
-  title => +{qw( 
-    data_type text 
-    is_nullable 0 
-  )},
-  snipet => {data_type => 'text'},
-  url => {data_type => 'text', is_nullable => 0},
-  cluster => +{qw(
-    data_type integer
-    is_nullable 0
-  )},
-  finished => {data_type => 'boolean', is_Nullable => 0},
+  id =>       +{qw|data_type integer  is_nullable 0  is_auto_increment  1|},
+  title =>    +{qw|data_type text     is_nullable 0|},
+  snipet =>   +{qw|data_type text                  |},
+  url =>      +{qw|data_type text     is_nullable 0|},
+  cluster =>  +{qw|data_type integer  is_nullable 0|},
+  finished => +{qw|data_type boolean  is_Nullable 0|},
 );
 
 __PACKAGE__->set_primary_key('id');
