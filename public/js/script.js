9c99b69c5428310e0ed677748e8cda1b3589126d
dryman <idryman@gmail.com>
2011-11-17 21:10:41 +0800

    lipsum and keydown event

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,30 @@
 /* Author: 
 
 */
+function checkKey(e){
+  var keycode = $.browser.ie ? e.KeyCode : e.which;
+  switch (keycode){
+    case 39: alert('right: next');break;
+    case 37: alert('left: previous (should we implement this?)'); break;
+    case 32: alert('You pressed whitespace!!'); break;
+    case 49: alert('按1表示一分鐘內看過'); break
+    case 50: alert('按2表示五分鐘內看過'); break
+    case 51: alert('按3表示十分鐘內看過'); break
+    }
+}
 
 
 
 
+$(function(){
+
+if ($.browser.mozilla){
+  $(document).keypress (checkKey);
+} else {
+  $(document).keydown (checkKey);
+}
+
+});
 
 
 
