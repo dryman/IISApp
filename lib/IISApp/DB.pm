0351ea5b8da42893565a09b828683b0857b919b1
dryman <idryman@gmail.com>
2011-11-21 10:28:00 +0800

    added README.mkdn

--- lib/IISApp/DB.pm
+++ lib/IISApp/DB.pm
@@ -3,3 +3,16 @@ use base 'DBIx::Class::Schema';
 IISApp::DB->load_namespaces;
 
 1;
+
+=head1 NAME
+
+IISApp::DB Perl object <-> relational database mapper.
+
+=head2 SYNOPSIS
+
+    use IISApp::DB;
+    my $schema = IISApp::DB->connect($dbi_dsn, $user, $pass);
+
+=cut
+
+__END__
